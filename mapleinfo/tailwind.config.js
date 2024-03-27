7c90728068fc5b666c49c55e650a2dacbcb9a6ae
hopak-e <a328028@gmail.com>
2024-03-27 18:55:51 +0900

    🍱 chore : 환경변수 추가 및 필요한 이미지 파일 추가

--- mapleinfo/tailwind.config.js
+++ mapleinfo/tailwind.config.js
@@ -1,8 +1,22 @@
 module.exports = {
   darkMode: "class",
+  mode: "jit",
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: {
+          50: "#11151C",
+          100: "#0A0D13",
+          150: "#586069",
+          200: "#292E36",
+          250: "#171B20",
+        },
+      },
+      backgroundImage: {
+        wallpaper: "url('./assets/wallpaper.svg')",
+      },
+    },
   },
   plugins: [],
 };
