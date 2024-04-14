fc584c1a0237cdeb18803aabdc7eb49d30923336
hopak-e <a328028@gmail.com>
2024-04-14 23:44:18 +0900

    📝 chore : 불필요한 요소 제거

--- mapleinfo/src/components/Header/Header.tsx
+++ mapleinfo/src/components/Header/Header.tsx
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 import useDarkMode, { useDark } from "../../hooks/useDarkMode";
 import useTabBtn, { useTab } from "../../hooks/useTab";
 import { Link } from "react-router-dom";
-
 import { ReactComponent as DarkMode } from "../../assets/darkmode.svg";
 import { ReactComponent as WhiteMode } from "../../assets/whitemode.svg";
 import SearchInput from "../inputs/SearchInput";
@@ -20,11 +19,9 @@ const Header = () => {
   const tabList: TabList[] = [
     { id: 0, name: "메인", link: "/" },
     { id: 1, name: "길드", link: "/guild" },
-    { id: 2, name: "랭킹", link: "/ranking" },
-    { id: 3, name: "큐브", link: "/cube" },
-    { id: 4, name: "스타포스", link: "/starforce" },
-    { id: 5, name: "본캐 찾기", link: "/trackChar" },
-    { id: 6, name: "계산기", link: "/calc" },
+    { id: 2, name: "큐브", link: "/cube" },
+    { id: 3, name: "스타포스", link: "/starforce" },
+    { id: 4, name: "본캐 찾기", link: "/trackChar" },
   ];
 
   useEffect(() => {
@@ -36,44 +33,50 @@ const Header = () => {
   }, [isDark]);
 
   return (
-    <header className="flex w-screen justify-center dark:bg-dark-100 dark:text-white">
-      <div className="flex flex-col w-full max-w-[1200px] ">
-        <div className="flex items-center w-full h-16 ">
-          <div className="text-xl w-1/6 cursor-pointer">MapleInfo</div>
-          <SearchInput placeholder="닉네임을 입력하세요" width="w-2/3" />
-          <div className="flex items-center h-full pl-6 w-1/6 ">
-            <div
-              className="flex items-center  justify-center w-12 h-12 hover:bg-slate-100 dark:hover:bg-white cursor-pointer rounded-full"
-              onClick={() => onToggleDarkMode("")}
-            >
-              {isDark ? <DarkMode /> : <WhiteMode />}
+    <header className="dark:bg-dark-100 dark:text-white">
+      <div className="grow shrink max-w-[1120px] mx-auto pt-2">
+        <div>
+          <div className="flex items-center gap-x-3 px-2">
+            <Link to="/">
+              <div className="text-md md:text-xl text-center cursor-pointer font-[700]">
+                MapleInfo
+              </div>
+            </Link>
+            <SearchInput placeholder="닉네임을 입력하세요" isDark={isDark} />
+            <div className="flex items-center">
+              <div
+                className="flex items-center  justify-center w-6 h-6 md:w-10 md:h-10 hover:bg-slate-100 dark:hover:bg-white cursor-pointer rounded-full"
+                onClick={() => onToggleDarkMode("")}
+              >
+                {isDark ? <DarkMode /> : <WhiteMode />}
+              </div>
             </div>
           </div>
-        </div>
-        <div className="flex w-full h-12">
-          <ul className="flex w-full h-full px-8">
-            {tabList.map((list: TabList) => (
-              <Link
-                to={list.link}
-                key={list.id}
-                className="flex grow h-full cursor-pointer"
-              >
-                <li
-                  className={`flex grow items-center h-full 
+          <div className="flex h-12">
+            <ul className="flex w-full px-8">
+              {tabList.map((list: TabList) => (
+                <Link
+                  to={list.link}
+                  key={list.id}
+                  className="flex grow h-full cursor-pointer"
+                >
+                  <li
+                    className={`flex grow items-center h-full 
                 ${
                   list.name === tab
                     ? "border-black border-b-4 dark:border-white "
                     : ""
                 }`}
-                  onClick={() => onToggleTab(list.name)}
-                >
-                  <span className="w-full text-center hover:bg-slate-100 hover:rounded-md dark:hover:bg-dark-250 ">
-                    {list.name}
-                  </span>
-                </li>
-              </Link>
-            ))}
-          </ul>
+                    onClick={() => onToggleTab(list.name)}
+                  >
+                    <span className="w-full text-center hover:bg-slate-100 hover:rounded-md dark:hover:bg-dark-250 ">
+                      {list.name}
+                    </span>
+                  </li>
+                </Link>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </header>
